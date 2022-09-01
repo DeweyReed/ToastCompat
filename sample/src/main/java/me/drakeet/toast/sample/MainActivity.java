be48c5d5a5c031b6bae21031d8376690d73eca1b
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2022-09-02 02:10:45 +0800

    Use AndroidX

--- sample/src/main/java/me/drakeet/toast/sample/MainActivity.java
+++ sample/src/main/java/me/drakeet/toast/sample/MainActivity.java
@@ -1,7 +1,7 @@
 package me.drakeet.toast.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 import me.drakeet.support.toast.ToastCompat;
